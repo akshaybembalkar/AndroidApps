61fdfd5d2b43898167ee325eb319ea21849f44ac
akshay_bembalkar <akshay6533@gmail.com>
2018-08-25 11:04:51 -0700

    provide current score and show game over on collision

--- RunNLiveMyFirstAndroidGame/app/src/main/java/com/akshayb/runnlive_myfirstandroidgame/GameEngine/GamePanel.java
+++ RunNLiveMyFirstAndroidGame/app/src/main/java/com/akshayb/runnlive_myfirstandroidgame/GameEngine/GamePanel.java
@@ -3,12 +3,14 @@ package com.akshayb.runnlive_myfirstandroidgame.GameEngine;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import com.akshayb.runnlive_myfirstandroidgame.Constants;
 import com.akshayb.runnlive_myfirstandroidgame.MainThread;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
@@ -16,6 +18,11 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private RectPlayer player;
     private Point playerPoint;
     private ObstacleManager obstacleManager;
+    private boolean movingPlayer = false;
+    private Rect r = new Rect();
+
+    private boolean isGameOver = false;
+    private long gameOverTime;
 
     public GamePanel(Context context) {
         super(context);
@@ -23,7 +30,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         getHolder().addCallback(this);
         mainThread = new MainThread(getHolder(), this);
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0 ));
-        playerPoint = new Point(150, 150);
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
+        player.update(playerPoint);
 
         obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
 
@@ -60,16 +68,43 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if (!isGameOver && player.getRectangle().contains((int)event.getX(), (int)event.getY())) {
+                    movingPlayer = true;
+                }
+                if (isGameOver && System.currentTimeMillis() - gameOverTime >= 2000) {
+                    reset();
+                    isGameOver = false;
+                }
+                break;
             case MotionEvent.ACTION_MOVE:
-                playerPoint.set((int)event.getX(), (int)event.getY());
+                if (movingPlayer && !isGameOver) {
+                    playerPoint.set((int) event.getX(), (int) event.getY());
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                 movingPlayer = false;
+                 break;
         }
         return true;
         // return super.onTouchEvent(event);
     }
 
-    public void update() {
+    public void reset() {
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
         player.update(playerPoint);
-        obstacleManager.update();
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+        movingPlayer = false;
+    }
+
+    public void update() {
+        if (!isGameOver) {
+            player.update(playerPoint);
+            obstacleManager.update();
+            if (obstacleManager.playerCollide(player)) {
+                isGameOver = true;
+                gameOverTime = System.currentTimeMillis();
+            }
+        }
     }
 
     @Override
@@ -78,5 +113,24 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         canvas.drawColor(Color.WHITE);
         player.draw(canvas);
         obstacleManager.draw(canvas);
+
+        if (isGameOver) {
+            Paint paint = new Paint();
+            paint.setTextSize(100);
+            paint.setColor(Color.MAGENTA);
+            drawCenterText(canvas, paint, "Game Over!");
+        }
     }
+
+    private void drawCenterText(Canvas canvas, Paint paint, String text) {
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.getClipBounds(r);
+        int cHeight = r.height();
+        int cWidth = r.width();
+        paint.getTextBounds(text, 0, text.length(), r);
+        float x = cWidth / 2f - r.width() / 2f - r.left;
+        float y = cHeight / 2f + r.height() / 2f - r.bottom;
+        canvas.drawText(text, x, y, paint);
+    }
+
 }
